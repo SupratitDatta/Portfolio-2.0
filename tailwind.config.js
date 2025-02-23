9921a180b4ad7a3558b2765eee40690f4cbb1e91
Supratit <supratitdatta@gmail.com>
2025-02-23 23:59:59 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { Config } from 'tailwindcss'
 
 const config = {
     content: [
+        './index.html", "./src/**/*.{js,ts,jsx,tsx}',
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}'
